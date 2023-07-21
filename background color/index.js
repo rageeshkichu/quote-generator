7835b957f8e5b67eaea0c913c1dca49d132ae17b
Ambadi <rageeshkichu@gmail.com>
2023-07-21 16:14:56 +0530

    ist commit

--- background color/index.js
+++
@@ -1,9 +0,0 @@
-var i= 0;
-function changecolors(){
-    var colors= ["red","blue","green","yellow"];
-    document.getElementsByTagName("body")[0].
-    style.background= colors [i++];
-if (i > colors.length - 1)
-i= 0;
-
-}
