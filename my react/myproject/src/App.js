1c5fff1afe28b1bd5038b8642b851010d8aeab3f
Ambadi <rageeshkichu@gmail.com>
2023-08-22 16:08:23 +0530

    ist commit

---
+++ my react/myproject/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Joke from './Joke'
+function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ my react/myproject/src/jokesData.js
@@ -0,0 +1,10 @@
+const jokeData=[
+      {question="what is ur name?"
+      punchline="my name is hero"},
+
+      {''}
+      question="fav book?"
+      punchline="none"
+      question="fav food?"
+      punchline="porotta"
+]
